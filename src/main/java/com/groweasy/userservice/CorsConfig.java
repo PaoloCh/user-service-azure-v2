fa4dce01250ce9d84c93ea46ae1ba0537a11477c
Paolo Sebastián Checa Apolinario <u202112749@upc.edu.pe>
2024-06-12 19:07:11 -0500

    update: application

---
+++ src/main/java/com/groweasy/userservice/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.groweasy.userservice;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*");
+    }
+}
