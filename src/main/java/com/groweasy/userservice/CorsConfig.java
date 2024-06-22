abdd24c6f31e971a555c7c7ea6247df4a5d6f25e
Paolo Sebastián Checa Apolinario <104078918+PaoloCh@users.noreply.github.com>
2024-06-22 10:14:34 -0500

    Update CorsConfig.java

--- src/main/java/com/groweasy/userservice/CorsConfig.java
+++ src/main/java/com/groweasy/userservice/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://grow-easy.azurewebsites.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*");
     }
